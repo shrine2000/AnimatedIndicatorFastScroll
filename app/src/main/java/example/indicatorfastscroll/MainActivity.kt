e899209f5aaa389ee817502062af2ff333482be0
John <shrine02sabu@gmail.com>
2021-01-12 13:23:31 +0530

    Updated UI

--- app/src/main/java/example/indicatorfastscroll/MainActivity.kt
+++ app/src/main/java/example/indicatorfastscroll/MainActivity.kt
@@ -1,15 +1,24 @@
 package example.indicatorfastscroll
 
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.pm.LauncherApps
 import android.os.Bundle
+import android.os.UserManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.afollestad.recyclical.datasource.DataSource
+import com.afollestad.recyclical.datasource.dataSourceTypedOf
 import com.afollestad.recyclical.setup
 import com.afollestad.recyclical.withItem
 import com.reddit.indicatorfastscroll.FastScrollItemIndicator
 import com.reddit.indicatorfastscroll.FastScrollerThumbView
 import com.reddit.indicatorfastscroll.FastScrollerView
+import java.util.*
+
+
+val appList: MutableList<AppModel> = mutableListOf()
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,13 +27,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val fastscroller = findViewById<FastScrollerView>(R.id.fastscroller)
-        val fastscroller_thumb = findViewById<FastScrollerThumbView>(R.id.fastscroller_thumb)
+        val fastScroller = findViewById<FastScrollerView>(R.id.fastscroller)
+        val fastScrollerThumb = findViewById<FastScrollerThumbView>(R.id.fastscroller_thumb)
 
-        fastscroller.setupWithRecyclerView(
+        populateList()
+
+        fastScroller.setupWithRecyclerView(
             recyclerView,
             { position ->
-                val item = getData()[position].title
+                val item = appList[position].appName
 
                 FastScrollItemIndicator.Text(
                     item.substring(0, 1).toUpperCase()
@@ -35,25 +46,60 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        fastscroller_thumb.setupWithFastScroller(fastscroller)
+        fastScrollerThumb.setupWithFastScroller(fastScroller)
 
 
         recyclerView.setup {
-            withDataSource(getData())
-            withItem<Model, ModelViewHolder>(R.layout.recyclerview_layout) {
+            withDataSource(appList.toDataSource())
+            withItem<AppModel, ModelViewHolder>(R.layout.recyclerview_layout) {
                 onBind(::ModelViewHolder) { index, item ->
-
-                    title.text = item.title
-                    subtitle.text = "${item.subtitle}"
+                    appName.text = item.appName
+                    appPackageName.text = item.appPackage
+                    appIcon.setImageDrawable(item.appIcon)
                 }
                 onClick { index ->
                     Toast.makeText(
                         this@MainActivity,
-                        getData()[index].toString(),
+                        appList[index].appPackage,
                         Toast.LENGTH_SHORT
                     ).show()
                 }
             }
         }
     }
+
+    private fun populateList() {
+
+        try {
+            val userManager = getSystemService(Context.USER_SERVICE) as UserManager
+            val launcherApps = getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
+
+            for (profile in userManager.userProfiles) {
+                for (app in launcherApps.getActivityList(null, profile)) {
+                    val appIcon = packageManager.getApplicationIcon(app.applicationInfo.packageName)
+                    appList.add(
+                        AppModel(
+                            app.label.toString(),
+                            app.applicationInfo.packageName,
+                            appIcon
+                        )
+                    )
+                }
+            }
+            appList.sortBy { it.appName.toLowerCase(Locale.ROOT) }
+
+        } catch (e: java.lang.Exception) {
+
+        }
+    }
 }
+
+
+private fun MutableList<AppModel>.toDataSource(): DataSource<AppModel> {
+    val dataSource = dataSourceTypedOf<AppModel>()
+    this.forEach {
+        dataSource.add(AppModel(it.appName, it.appPackage, it.appIcon))
+    }
+    return dataSource
+}
+
