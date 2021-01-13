7a4cdc2edb534a354cf70fe26a0f30a027ee1238
John <shrine02sabu@gmail.com>
2021-01-13 13:16:02 +0530

    added onTouchListener

--- app/src/main/java/example/indicatorfastscroll/MainActivity.kt
+++ app/src/main/java/example/indicatorfastscroll/MainActivity.kt
@@ -1,6 +1,7 @@
 package example.indicatorfastscroll
 
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
@@ -8,17 +9,16 @@ import android.content.pm.LauncherApps
 import android.graphics.Typeface
 import android.os.Bundle
 import android.os.UserManager
-import android.widget.Button
-import android.widget.Toast
+import android.util.Log
+import android.view.DragEvent
+import android.view.MotionEvent
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.afollestad.recyclical.datasource.DataSource
-import com.afollestad.recyclical.datasource.dataSourceTypedOf
 import com.afollestad.recyclical.setup
 import com.afollestad.recyclical.withItem
-import com.reddit.indicatorfastscroll.FastScrollItemIndicator
-import com.reddit.indicatorfastscroll.FastScrollerThumbView
-import com.reddit.indicatorfastscroll.FastScrollerView
+import com.reddit.indicatorfastscroll.*
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
@@ -28,33 +28,29 @@ import java.util.*
 
 val appList: MutableList<AppModel> = mutableListOf()
 lateinit var recyclerView: RecyclerView
+lateinit var fastScroller: FastScrollerView
 
-class MainActivity : AppCompatActivity() {
+
+var X = 0f
+var Y = 0f
+
+
+
+open class MainActivity : AppCompatActivity() {
+
+    private val TAG ="mmxx"
+
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val fastScroller = findViewById<FastScrollerView>(R.id.fastscroller)
+        fastScroller = findViewById<FastScrollerView>(R.id.fastscroller)
         val fastScrollerThumb = findViewById<FastScrollerThumbView>(R.id.fastscroller_thumb)
         val appPreferences = AppPreferences(this)
         populateList()
-
-        fastScroller.setupWithRecyclerView(
-            recyclerView,
-            { position ->
-                val item = appList[position].appName
-
-                FastScrollItemIndicator.Text(
-                    item.substring(0, 1).toUpperCase()
-                )
-            },
-
-            )
-
-
-
-        fastScrollerThumb.setupWithFastScroller(fastScroller)
 
 
         recyclerView.setup {
@@ -74,9 +70,9 @@ class MainActivity : AppCompatActivity() {
                 }
                 onClick { index ->
                     Toast.makeText(
-                        this@MainActivity,
-                        appList[index].appPackage,
-                        Toast.LENGTH_SHORT
+                            this@MainActivity,
+                            appList[index].appPackage,
+                            Toast.LENGTH_SHORT
                     ).show()
                 }
             }
@@ -85,9 +81,78 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.button_select_font).setOnClickListener {
             selectFontPath()
-
         }
+
+
+        var widgetXOrigin : Float = 0F
+        var widgetYOrigin : Float = 0F
+        var widgetDX: Float = 0F
+
+        var widgetDY: Float = 0F
+
+        fastScroller.setOnTouchListener { v, event ->
+
+            val viewParent:View = (v.parent as View)
+            val PARENT_HEIGHT = viewParent.height
+            val PARENT_WIDTH = viewParent.width
+
+            when(event.actionMasked){
+                MotionEvent.ACTION_DOWN -> {
+                    widgetDX = v.x - event.rawX
+                    widgetDY = v.y - event.rawY
+                    // save widget origin coordinate
+                    widgetXOrigin = v.x
+                    widgetYOrigin = v.y
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    // Screen border Collision
+                    var newX = event.rawX + widgetDX
+                    newX = 0F.coerceAtLeast(newX)
+                    newX = (PARENT_WIDTH - v.width).toFloat().coerceAtMost(newX)
+                    v.x = newX
+
+                    var newY = event.rawY + widgetDY
+                    newY = 0F.coerceAtLeast(newY)
+                    newY = (PARENT_HEIGHT - v.height).toFloat().coerceAtMost(newY)
+                    v.y = newY
+
+
+                }
+                MotionEvent.ACTION_UP -> {
+                    // Back to original position
+                    v.x = widgetXOrigin
+                    v.y = widgetYOrigin
+
+                    v.animate().x(widgetXOrigin).setDuration(250).start()
+                    v.animate().y(widgetYOrigin).setDuration(250).start()
+
+                }
+                else -> {
+                    return@setOnTouchListener false
+                }
+            }
+            true
+        }
+
+        fastScroller.setupWithRecyclerView(
+                recyclerView,
+                { position ->
+
+                    val item = appList[position].appName
+
+                    FastScrollItemIndicator.Text(
+                            item.substring(0, 1).toUpperCase()
+                    )
+                },
+
+                )
+
+
+        fastScrollerThumb.setupWithFastScroller(fastScroller)
+
+
     }
+
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -126,45 +191,45 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    private fun populateList() {
 
-        private fun populateList() {
+        try {
+            val userManager = getSystemService(Context.USER_SERVICE) as UserManager
+            val launcherApps = getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
 
-            try {
-                val userManager = getSystemService(Context.USER_SERVICE) as UserManager
-                val launcherApps = getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
-
-                for (profile in userManager.userProfiles) {
-                    for (app in launcherApps.getActivityList(null, profile)) {
-                        val appIcon = packageManager.getApplicationIcon(app.applicationInfo.packageName)
-                        appList.add(
-                                AppModel(
-                                        app.label.toString(),
-                                        app.applicationInfo.packageName,
-                                        appIcon
-                                )
-                        )
-                    }
+            for (profile in userManager.userProfiles) {
+                for (app in launcherApps.getActivityList(null, profile)) {
+                    val appIcon = packageManager.getApplicationIcon(app.applicationInfo.packageName)
+                    appList.add(
+                            AppModel(
+                                    app.label.toString(),
+                                    app.applicationInfo.packageName,
+                                    appIcon
+                            )
+                    )
                 }
-                appList.sortBy { it.appName.toLowerCase(Locale.ROOT) }
-
-            } catch (e: java.lang.Exception) {
-
             }
+            appList.sortBy { it.appName.toLowerCase(Locale.ROOT) }
+
+        } catch (e: java.lang.Exception) {
+
         }
-
-        private fun selectFontPath() {
-
-            Toast.makeText(applicationContext,"Download ttf fonts & select them", Toast.LENGTH_LONG).show()
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-                type = "*/*"
-                addCategory(Intent.CATEGORY_OPENABLE)
-                flags = flags or Intent.FLAG_GRANT_READ_URI_PERMISSION
-            }
-
-            startActivityForResult(intent, OPEN_DOCUMENT_REQUEST_CODE)
-        }
-
     }
+
+    private fun selectFontPath() {
+
+        Toast.makeText(applicationContext, "Download ttf fonts & select them", Toast.LENGTH_LONG).show()
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            type = "*/*"
+            addCategory(Intent.CATEGORY_OPENABLE)
+            flags = flags or Intent.FLAG_GRANT_READ_URI_PERMISSION
+        }
+
+        startActivityForResult(intent, OPEN_DOCUMENT_REQUEST_CODE)
+    }
+
+
+}
 
 
 
--- app/src/main/java/example/indicatorfastscroll/Utils.kt
+++ app/src/main/java/example/indicatorfastscroll/Utils.kt
@@ -3,19 +3,22 @@ package example.indicatorfastscroll
 
 import android.graphics.drawable.Drawable
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.afollestad.recyclical.ViewHolder
 import com.afollestad.recyclical.datasource.DataSource
 import com.afollestad.recyclical.datasource.dataSourceTypedOf
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 
 const val OPEN_DOCUMENT_REQUEST_CODE = 345
 
 data class AppModel(
-    var appName: String,
-    var appPackage: String,
-    var appIcon: Drawable
+        var appName: String,
+        var appPackage: String,
+        var appIcon: Drawable
 )
 
 
@@ -36,3 +39,51 @@ fun MutableList<AppModel>.toDataSource(): DataSource<AppModel> {
 }
 
 fun String.isNotEmpty() = this != ""
+
+
+fun getAllChildren(v: View): ArrayList<View> {
+    if (v !is ViewGroup) {
+        val viewArrayList = ArrayList<View>()
+        viewArrayList.add(v)
+        return viewArrayList
+    }
+    val result = ArrayList<View>()
+    for (i in 0 until v.childCount) {
+        val child = v.getChildAt(i)
+        val viewArrayList = ArrayList<View>()
+        viewArrayList.add(v)
+        viewArrayList.addAll(getAllChildren(child))
+        result.addAll(viewArrayList)
+    }
+    return result
+}
+
+class UpdateDelegate<T>(val update: (T) -> Unit) : ReadWriteProperty<Any?, T> {
+
+    var set = false
+    var value: T? = null
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (!set) {
+            throw IllegalStateException("Property ${property.name} should be initialized before get.")
+        }
+        @Suppress("UNCHECKED_CAST")
+        return value as T
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        val wasSet = set
+        this.set = true
+        this.value = value
+        if (wasSet) {
+            update(value)
+        }
+    }
+}
+
+/**
+ * A delegate that sets a backing value and calls [update] on every change after the first.
+ */
+internal fun <T> onUpdate(update: (T) -> Unit) = UpdateDelegate(update)
+
+internal fun <T> onUpdate(update: () -> Unit) = UpdateDelegate<T> { update() }
